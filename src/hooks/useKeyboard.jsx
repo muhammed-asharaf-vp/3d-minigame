b6a88343ae025f615b1bee016bec67d27b538067
muhammed-asharaf-vp <mohamedasharafvpp@gmail.com>
2026-01-08 13:32:53 +0530

    update

--- src/hooks/useKeyboard.jsx
+++ src/hooks/useKeyboard.jsx
@@ -1,34 +1,4 @@
-// import { useEffect, useState } from "react";
 
-// export default function useKeyboard() {
-//   const [keys, setKeys] = useState({});
-
-//   useEffect(() => {
-//     const handleKeyDown = (e) => {
-//       setKeys((prev) => ({ ...prev, [e.code]: true }));
-//     };
-
-//     const handleKeyUp = (e) => {
-//       setKeys((prev) => ({ ...prev, [e.code]: false }));
-//     };
-
-//     window.addEventListener("keydown", handleKeyDown);
-//     window.addEventListener("keyup", handleKeyUp);
-
-//     return () => {
-//       window.removeEventListener("keydown", handleKeyDown);
-//       window.removeEventListener("keyup", handleKeyUp);
-//     };
-//   }, []);
-
-//   return {
-//     forward: !!keys["KeyW"],
-//     backward: !!keys["KeyS"],
-//     left: !!keys["KeyA"],
-//     right: !!keys["KeyD"],
-//     interact: !!keys["KeyE"],
-//   };
-// }
 
 
 import { useEffect, useState } from "react";
